a5e79131a35e120d1a660631d320959806b78910
davips <dpsabc@gmail.com>
2016-12-23 16:14:37 -0200

    bmp

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,17 @@
-object Main extends Arg {
+import java.io.File
+import javax.imageio.ImageIO
 
+object Main extends Arg {
+  val img = ImageIO.read(new File("example.bmp"))
+  val green = Array.fill(img.getHeight)(Array.fill(img.getWidth)(new Byte))
+  for {
+    y <- 0 until img.getHeight
+    x <- 0 until img.getWidth
+  } {
+    val color = img.getRGB(x, y)
+    //alpha[x][y] = (byte)(color>>24);
+    //red[x][y] = (byte)(color>>16);
+    green(x)(y) = (color >> 8).toByte
+    //blue[x][y] = (byte)(color);
+  }
 }
