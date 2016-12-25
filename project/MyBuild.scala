0fd0e1e271cf5282c593d5fb55630cab49c77231
davips <davips>
2016-12-25 17:34:55 -0200

    outsourced subimage

--- project/MyBuild.scala
+++ project/MyBuild.scala
@@ -4,8 +4,12 @@ import Keys._
 object MyBuild extends Build {
 
   lazy val root: Project = Project("image2arff", file("."))
-    .dependsOn(p1)
-  lazy val p1 = ProjectRef(uri("https://github.com/davips/args.git"), "args")
+//    .aggregate(subimage, args)
+    .dependsOn(args)
+    .dependsOn(subimage)
+
+  lazy val args = ProjectRef(uri("https://github.com/davips/args.git"), "args")
+  lazy val subimage = ProjectRef(uri("https://github.com/davips/subimage.git"), "subimage")
 
 }
 
